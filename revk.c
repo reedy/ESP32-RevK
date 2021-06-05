1058065e894a472df3c94f39d905d5c1c14ebf50
Adrian Kennard <a@k.gg>
2021-06-05 11:01:10 +0100

    Debug

--- revk.c
+++ revk.c
@@ -1709,6 +1709,7 @@ static const char *revk_setting_dump(void)
          }
          void addvalue(const char *tag, int n) {        // Add a value
             start();
+	    ESP_LOGI(TAG,"Adding %s(%d)",tag?:"?",n); // TODO
             void *data = s->data;
             if (!(s->flags & SETTING_BOOLEAN))
                data += (s->size ? : sizeof(void *)) * n;        // TODO
