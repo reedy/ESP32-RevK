53cefc58e7fbdd8dd824f687766efcbfe269ef10
Adrian Kennard <a@k.gg>
2021-07-01 11:52:34 +0100

    LWMQTT

--- include/lwmqtt.h
+++ include/lwmqtt.h
@@ -14,40 +14,46 @@
 // Called for disconnect
 // - Topic is NULL
 // - Payload is NULL
-typedef void lwmqtt_callback_t(void*arg,const char *topic,size_t len,const unsigned char*payload);
+typedef void lwmqtt_callback_t(void *arg, const char *topic, unsigned short len, const unsigned char *payload);
 
 typedef struct lwmqtt_config_s lwmqtt_config_t;
 
 // Config for connection
-struct lwmqtt_config_s
-{
-	const char *host;
-	int port;
-	lwmqtt_callback_t*callback;
-	void *arg;
-	char *client;
-	char *username;
-	char *password;
-	const char *topic;	// Will topic
-	size_t len;		// Will payload len
-	const unsigned char *payload;	// Will payload
-	// TLS settings TODO
-	uint8_t retain:1;	// Will retain
+struct lwmqtt_config_s {
+   const char *host;            // Name or IP
+   const char *port;            // Name or port number
+   lwmqtt_callback_t *callback;
+   void *arg;
+   char *client;
+   char *username;
+   char *password;
+   const char *topic;           // Will topic
+   int plen;                    // Will payload len (-1 does strlen)
+   const unsigned char *payload;        // Will payload
+   unsigned short keepalive;
+   // TLS settings TODO
+   uint8_t retain:1;            // Will retain
 };
 
 // Handle for connection
 typedef struct lwmqtt_handle_s *lwmqtt_handle_t;
 
 // Create a connection (NULL if failed)
-lwmqtt_handle_t lwmqtt_init(lwmqtt_config_t*);
+lwmqtt_handle_t lwmqtt_init(lwmqtt_config_t *);
 
 // End connection - actually freed later as part of task. Will do a callback when closed if was connected
 // NULLs the passed handle - do not use handle after this call
-void lwmqtt_end(lwmqtt_handle_t*);
+void lwmqtt_end(lwmqtt_handle_t *);
 
 // Subscribe (return is non null error message if failed)
-const char * lwmqtt_subscribe(lwmqtt_handle_t,const char *topic);
-const char * lwmqtt_unsubscribe(lwmqtt_handle_t,const char *topic);
+const char *lwmqtt_subscribeub(lwmqtt_handle_t, const char *topic, char unsubscribe);
+#define lwmqtt_subscribe(h,t) lwmqtt_subscribeub(h,t,0);
+#define lwmqtt_unsubscribe(h,t) lwmqtt_subscribeub(h,t,0);
 
-// Send (return is non null error message if failed)
-const char * lwmqtt_send(lwmqtt_handle_t,const char *topic,size_t len,const unsigned char *payload,char retail);
+// Send (return is non null error message if failed) (-1 tlen or plen do strlen)
+const char *lwmqtt_send_full(lwmqtt_handle_t, int tlen, const char *topic, int plen, const unsigned char *payload, char retain, char nowait);
+// Simpler
+#define lwmqtt_send(h,t,l,p) lwmqtt_send_full(h,-1,t,l,p,0,0);
+
+// Simple send - non retained no wait topic ends on space then payload
+const char *lwmqtt_send_str(lwmqtt_handle_t, const char *msg);
--- lwmqtt.c
+++ lwmqtt.c
@@ -3,6 +3,8 @@
 // Live sending to TCP for outgoing messages
 // Simple callback for incoming messages
 // Automatic reconnect
+static const char
+    __attribute__((unused)) * TAG = "LWMQTT";
 
 #include <stdio.h>
 #include <stdint.h>
@@ -15,6 +17,7 @@
 #include "nvs_flash.h"
 #include "esp_event.h"
 #include "esp_timer.h"
+#include "esp_log.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -35,13 +38,18 @@
 
 struct lwmqtt_handle_s {
    char *host;                  // malloc'd host name
-   int port;
+   char *port;                  // malloc'd port name
    lwmqtt_callback_t *callback;
    void *arg;
-   size_t connectlen;
+   int connectlen;
    unsigned char *connect;      // malloc'd connect message
-   // Mutex TODO
-   // TLS settings TODO
+   SemaphoreHandle_t mutex;     // atomic send mutex
+   int sock;                    // TCP socket
+   unsigned short keepalive;
+   unsigned short seq;
+   time_t ka;
+   uint8_t running:1;
+   // TODO TLS
 };
 
 static void task(void *pvParameters);
@@ -55,34 +63,49 @@ lwmqtt_handle_t lwmqtt_init(lwmqtt_config_t * config)
    if (!handle)
       return NULL;
    memset(handle, 0, sizeof(*handle));
+   handle->sock = -1;
+   handle->callback = config->callback;
+   handle->arg = config->arg;
+   handle->keepalive = config->keepalive ? : 60;
    handle->host = strdup(config->host);
    if (!handle->host)
    {
       free(handle);
       return NULL;
    }
-   handle->port = (config->port ? : 1883);
-   // Make connection message
-   size_t len = 6 + 1 + 1 + 2 + strlen(config->client ? : "");
-   if (config->topic)
-      len += 2 + strlen(config->topic) + 2 + config->len;
-   if (config->username)
-      len += 2 + strlen(config->username);
-   if (config->password)
-      len += 2 + strlen(config->password);
-   if (len >= 128 * 128)
-   {                            // Nope
+   handle->port = strdup(config->port ? : "1883");
+   if (!handle->port)
+   {
       free(handle->host);
       free(handle);
       return NULL;
    }
-   if (len >= 128)
-      len++;                    // two byte len
-   len += 2;                    // header and one byte len
-   handle->connect = malloc(len);
+   // Make connection message
+   int mlen = 6 + 1 + 1 + 2 + strlen(config->client ? : "");
+   if (config->plen < 0)
+      config->plen = strlen((char *) config->payload ? : "");
+   if (config->topic)
+      mlen += 2 + strlen(config->topic) + 2 + config->plen;
+   if (config->username)
+      mlen += 2 + strlen(config->username);
+   if (config->password)
+      mlen += 2 + strlen(config->password);
+   if (mlen >= 128 * 128)
+   {                            // Nope
+      free(handle->host);
+      free(handle->port);
+      free(handle);
+      return NULL;
+   }
+   mlen += 2;                   // keepalive
+   if (mlen >= 128)
+      mlen++;                   // two byte len
+   mlen += 2;                   // header and one byte len
+   handle->connect = malloc(mlen);
    if (!handle->connect)
    {
       free(handle->host);
+      free(handle->port);
       free(handle);
       return NULL;
    }
@@ -97,13 +120,12 @@ lwmqtt_handle_t lwmqtt_init(lwmqtt_config_t * config)
       p += l;
    }
    *p++ = 0x10;                 // connect
-   if (len > 129)
+   if (mlen > 129)
    {                            // Two byte len
-      *p++ = (((len - 3) & 0x7F) | 0x80);
-      *p++ = ((len - 3) >> 7);
+      *p++ = (((mlen - 3) & 0x7F) | 0x80);
+      *p++ = ((mlen - 3) >> 7);
    } else
-      *p++ = len - 2;           // 1 byte len
-   *p++ = 0;                    // protocol name
+      *p++ = mlen - 2;          // 1 byte len
    str(4, "MQTT");
    *p++ = 4;                    // protocol level
    *p = 0x02;                   // connect flags (clean)
@@ -115,22 +137,28 @@ lwmqtt_handle_t lwmqtt_init(lwmqtt_config_t * config)
    {
       *p |= 0x04;               // Will
       if (config->retain)
-         *p |= 0x40;            // Will retain
+         *p |= 0x20;            // Will retain
    }
    p++;
+   *p++ = handle->keepalive >> 8;       // keep alive
+   *p++ = handle->keepalive;
    str(-1, config->client);     // Client ID
    if (config->topic)
    {                            // Will
       str(-1, config->topic);   // Topic
-      str(config->len, (void *) config->payload);       // Payload
+      str(config->plen, (void *) config->payload);      // Payload
    }
    if (config->username)
       str(-1, config->username);
    if (config->password)
       str(-1, config->password);
+   handle->connectlen = mlen;
+   handle->mutex = xSemaphoreCreateBinary();
+   xSemaphoreGive(handle->mutex);
    TaskHandle_t task_id = NULL;
+   handle->running = 1;
    xTaskCreate(task, "mqtt", 8 * 1024, (void *) handle, 2, &task_id);
-   return NULL;
+   return handle;
 }
 
 // End connection - actually freed later as part of task. Will do a callback when closed if was connected
@@ -139,44 +167,369 @@ void lwmqtt_end(lwmqtt_handle_t * handle)
 {
    if (!handle || !*handle)
       return;
-   (*handle)->port = 0;         // Tells server closed
+   (*handle)->running = 0;
+   *handle = NULL;
 }
 
 // Subscribe (return is non null error message if failed)
-const char *lwmqtt_subscribe(lwmqtt_handle_t handle, const char *topic)
+const char *lwmqtt_subscribeub(lwmqtt_handle_t handle, const char *topic, char unsubscribe)
 {
-   return "TODO";
-}
-
-const char *lwmqtt_unsubscribe(lwmqtt_handle_t handle, const char *topic)
-{
-   return "TODO";
+   const char *ret = NULL;
+   if (!handle)
+      ret = "No handle";
+   else
+   {
+      int tlen = strlen(topic ? : "");
+      int mlen = 2 + 2 + tlen;
+      if (!unsubscribe)
+         mlen++;                // QoS requested
+      if (mlen >= 128 * 128)
+         ret = "Too big";
+      else
+      {
+         if (mlen >= 128)
+            mlen++;             // two byte len
+         mlen += 2;             // header and one byte len
+         unsigned char *buf = malloc(mlen);
+         if (!buf)
+            ret = "Malloc";
+         else
+         {
+            if (!xSemaphoreTake(handle->mutex, portMAX_DELAY))
+               ret = "Failed to get lock";
+            else
+            {
+               if (handle->sock < 0)
+                  ret = "Not connected";
+               else
+               {
+                  unsigned char *p = buf;
+                  *p++ = (unsubscribe ? 0xA2 : 0x82);   // subscribe/unsubscribe
+                  if (mlen > 129)
+                  {             // Two byte len
+                     *p++ = (((mlen - 3) & 0x7F) | 0x80);
+                     *p++ = ((mlen - 3) >> 7);
+                  } else
+                     *p++ = mlen - 2;   // 1 byte len
+                  handle->seq++;
+                  *p++ = handle->seq >> 8;
+                  *p++ = handle->seq;
+                  *p++ = tlen >> 8;
+                  *p++ = tlen;
+                  if (tlen)
+                     memcpy(p, topic, tlen);
+                  p += tlen;
+                  if (!unsubscribe)
+                     *p++ = 0x00;       // QoS requested
+                  if (send(handle->sock, buf, mlen, 0) < mlen)
+                     ret = "Failed to send";
+                  else
+                     handle->ka = time(0) + handle->keepalive;
+               }
+               xSemaphoreGive(handle->mutex);
+            }
+            free(buf);
+         }
+      }
+   }
+   if (ret)
+      ESP_LOGI(TAG, "Sub/unsub: %s", ret);
+   return ret;
 }
 
 // Send (return is non null error message if failed)
-const char *lwmqtt_send(lwmqtt_handle_t handle, const char *topic, size_t len, const unsigned char *payload, char retain)
+const char *lwmqtt_send_full(lwmqtt_handle_t handle, int tlen, const char *topic, int plen, const unsigned char *payload, char retain, char nowait)
 {
-   return "TODO";
+   const char *ret = NULL;
+   if (!handle)
+      ret = "No handle";
+   else
+   {
+      if (tlen < 0)
+         tlen = strlen(topic ? : "");
+      if (plen < 0)
+         plen = strlen((char *) payload ? : "");
+      int mlen = 2 + tlen + plen;
+      if (mlen >= 128 * 128)
+         ret = "Too big";
+      else
+      {
+         if (mlen >= 128)
+            mlen++;             // two byte len
+         mlen += 2;             // header and one byte len
+         unsigned char *buf = malloc(mlen);
+         if (!buf)
+            ret = "Malloc";
+         else
+         {
+            if (!xSemaphoreTake(handle->mutex, nowait ? 1 : portMAX_DELAY))
+               ret = "Failed to get lock";
+            else
+            {
+               if (handle->sock < 0)
+                  ret = "Not connected";
+               else
+               {
+                  unsigned char *p = buf;
+                  *p++ = 0x30 + (retain ? 1 : 0);       // message
+                  if (mlen > 129)
+                  {             // Two byte len
+                     *p++ = (((mlen - 3) & 0x7F) | 0x80);
+                     *p++ = ((mlen - 3) >> 7);
+                  } else
+                     *p++ = mlen - 2;   // 1 byte len
+                  *p++ = tlen >> 8;
+                  *p++ = tlen;
+                  if (tlen)
+                     memcpy(p, topic, tlen);
+                  p += tlen;
+                  if (plen && payload)
+                     memcpy(p, payload, plen);
+                  p += plen;
+                  if (send(handle->sock, buf, mlen, nowait ? MSG_DONTWAIT : 0) < mlen)
+                     ret = "Failed to send";
+                  else
+                     handle->ka = time(0) + handle->keepalive;
+               }
+               xSemaphoreGive(handle->mutex);
+            }
+            free(buf);
+         }
+      }
+   }
+   if (ret)
+      ESP_LOGI(TAG, "Send: %s", ret);
+   return ret;
 }
 
 static void task(void *pvParameters)
 {
    lwmqtt_handle_t handle = pvParameters;
-   int port;                    // port gets cleared to indicate close wanted
-   while ((port = handle->port))
+   int backoff = 1;
+   while (handle->running)
    {
       // Connect
+      ESP_LOGI(TAG, "Connecting %s:%s", handle->host, handle->port);
+      int sock = -1;
+      {
+         const struct addrinfo hints = {
+            .ai_family = AF_UNSPEC,
+            .ai_socktype = SOCK_STREAM,
+         };
+         struct addrinfo *a = NULL;
+         if (getaddrinfo(handle->host, handle->port, &hints, &a) || !a)
+         {
+            ESP_LOGE(TAG, "Failed to get address");
+            sleep(10);
+            continue;
+         }
+         for (struct addrinfo * p = a; p; p = p->ai_next)
+         {
+            sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+            if (sock < 0)
+               continue;
+            if (connect(sock, p->ai_addr, p->ai_addrlen))
+            {
+               close(sock);
+               sock = -1;
+               continue;
+            }
+            break;
+         }
+         freeaddrinfo(a);
+      }
+      if (sock < 0)
+         ESP_LOGI(TAG, "Cannot connect");
+      else
+      {
+         int len = send(sock, handle->connect, handle->connectlen, 0);
+         if (len < 0)
+            ESP_LOGE(TAG, "Failed to send connect");
+         else
+         {
 
-      // TODO
-
-      // Handle rx messages
-
-      // TODO
-
-      sleep(1);                 // Wait a mo
+            handle->sock = sock;
+            // Handle rx messages
+            unsigned char *buf = 0;
+            int buflen = 0;
+            int pos = 0;
+            handle->ka = time(0) + handle->keepalive;
+            while (handle->running)
+            {
+               int need = 0;
+               if (pos < 2)
+                  need = 2;
+               else if (!(buf[1] & 0x80))
+                  need = 2 + buf[1];    // One byte len
+               else if (pos < 3)
+                  need = 3;
+               else if (!(buf[2] & 0x80))
+                  need = 3 + (buf[2] >> 7) + (buf[1] & 0x7F);   // Two byte len
+               else
+               {
+                  ESP_LOGI(TAG, "Silly len %02X %02X %02X", buf[0], buf[1], buf[2]);
+                  break;
+               }
+               if (pos < need)
+               {
+                  time_t now = time(0);
+                  if (now >= handle->ka)
+                  {
+                     uint8_t b[] = { 0xC0, 0x00 };      // Ping
+                     xSemaphoreTake(handle->mutex, portMAX_DELAY);
+                     if (send(sock, b, sizeof(b), MSG_DONTWAIT) == sizeof(b))
+                        handle->ka = time(0) + handle->keepalive;
+                     xSemaphoreGive(handle->mutex);
+                  }
+                  fd_set r;
+                  FD_ZERO(&r);
+                  FD_SET(sock, &r);
+                  struct timeval to = { (now < handle->ka) ? (handle->ka - now) : 1, 0 };
+                  int sel = select(sock + 1, &r, NULL, NULL, &to);
+                  if (sel < 0)
+                     break;
+                  if (sel)
+                  {
+                     if (need > buflen)
+                     {
+                        buf = realloc(buf, (buflen = need) + 1);        // One more to allow extra null on end in all cases
+                        if (!buf)
+                        {
+                           ESP_LOGE(TAG, "realloc fail %d", need);
+                           break;
+                        }
+                     }
+                     int got = read(sock, buf + pos, need - pos);
+                     if (got <= 0)
+                        break;  // Error or close
+                     pos += got;
+                  }
+                  continue;
+               }
+               unsigned char *p = buf + 1,
+                   *e = buf + pos;
+               while (p < e && (*p & 0x80))
+                  p++;
+               p++;
+               switch (*buf >> 4)
+               {
+               case 2:         // conack
+                  ESP_LOGI(TAG, "Connected");
+                  backoff = 1;
+                  if (handle->callback)
+                     handle->callback(handle->arg, NULL, strlen(handle->host), (void *) handle->host);
+                  break;
+               case 3:         // pub
+                  {
+                     // Topic
+                     int tlen = (p[0] << 8) + p[1];
+                     p += 2;
+                     char *topic = (char *) p;
+                     p += tlen;
+                     unsigned short id = 0;
+                     if (*buf & 0x06)
+                     {
+                        id = (p[0] << 8) + p[1];
+                        p += 2;
+                     }
+                     if (p > e)
+                     {
+                        ESP_LOGE(TAG, "Bad msg");
+                        break;
+                     }
+                     if (*buf & 0x60)
+                     {          // reply
+                        uint8_t b[4] = { (*buf & 0x4) ? 0x50 : 0x40, 2, id >> 8, id };
+                        xSemaphoreTake(handle->mutex, portMAX_DELAY);
+                        if (send(sock, b, sizeof(b), MSG_DONTWAIT) == sizeof(b))
+                           handle->ka = time(0) + handle->keepalive;
+                        xSemaphoreGive(handle->mutex);
+                     }
+                     int plen = e - p;
+                     if (handle->callback)
+                     {
+                        if (plen && !(*buf & 0x60))
+                        {       // Move back a byte for null termination to be added without hitting payload
+                           memmove(topic - 1, topic, tlen);
+                           topic--;
+                        }
+                        topic[tlen] = 0;
+                        p[plen] = 0;
+                        handle->callback(handle->arg, topic, plen, p);
+                     }
+                  }
+                  break;
+               case 4:         // puback - not expected
+                  break;
+               case 5:         // pubrec - not expected
+                  {
+                     uint8_t b[4] = { 0x60, p[0], p[1] };
+                     xSemaphoreTake(handle->mutex, portMAX_DELAY);
+                     if (send(sock, b, sizeof(b), MSG_DONTWAIT) == sizeof(b))
+                        handle->ka = time(0) + handle->keepalive;
+                     xSemaphoreGive(handle->mutex);
+                  }
+                  break;
+               case 6:         // pubcomp - not expected
+                  break;
+               case 9:         // suback - ok
+                  break;
+               case 11:        // unsuback - ok
+                  break;
+               case 13:        // pingresp - ok
+                  break;
+               default:
+                  ESP_LOGI(TAG, "Unknown MQTT %02X", *buf);
+               }
+               pos = 0;
+            }
+            if (buf)
+               free(buf);
+            if (handle->callback)
+               handle->callback(handle->arg, NULL, 0, NULL);
+         }
+         // Close connection
+         xSemaphoreTake(handle->mutex, portMAX_DELAY);
+         if (!handle->running)
+         {                      // Closed
+            ESP_LOGI(TAG, "Close cleanly");
+            uint8_t b[] = { 0xE0, 0x00 };       // Disconnect cleanly
+            xSemaphoreTake(handle->mutex, portMAX_DELAY);
+            if (send(sock, b, sizeof(b), MSG_DONTWAIT) == sizeof(b))
+               handle->ka = time(0) + handle->keepalive;
+            xSemaphoreGive(handle->mutex);
+            if (handle->callback)
+               handle->callback(handle->arg, NULL, 0, NULL);
+         }
+         handle->sock = -1;
+         shutdown(sock, 0);
+         close(sock);
+         xSemaphoreGive(handle->mutex);
+      }
+      if (backoff < 60)
+         backoff *= 2;
+      ESP_LOGI(TAG, "Waiting %d", backoff);
+      sleep(backoff);
    }
+   vSemaphoreDelete(handle->mutex);
    free(handle->host);
+   free(handle->port);
    free(handle->connect);
    free(handle);
    vTaskDelete(NULL);
+}
+
+// Simple send - non retained no wait topic ends on space then payload
+const char *lwmqtt_send_str(lwmqtt_handle_t handle, const char *msg)
+{
+   if (!handle || !*msg)
+      return NULL;
+   const char *p;
+   for (p = msg; *p && *p != '\t'; p++);
+   if (!*p)
+      for (p = msg; *p && *p != ' '; p++);
+   int tlen = p - msg;
+   if (*p)
+      p++;
+   return lwmqtt_send_full(handle, tlen, msg, strlen(p), (void *) p, 0, 1);
 }
