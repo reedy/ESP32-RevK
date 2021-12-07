6e391062c1796e81cb94753127d105fa4e8e759a
Adrian Kennard <a@k.gg>
2021-12-07 15:00:37 +0000

    Grr

--- revk.c
+++ revk.c
@@ -1153,7 +1153,7 @@ static void ip_event_handler(void *arg, esp_event_base_t event_base, int32_t eve
             ESP_LOGI(TAG, "Got IP " IPSTR " from %s", IP2STR(&event->ip_info.ip), (char *) ap.ssid);
             xEventGroupSetBits(revk_group, GROUP_IP);
             sntp_stop();
-            sntp_start();
+            sntp_init();
 #ifdef	CONFIG_REVK_MQTT
             revk_mqtt_init();
 #endif
