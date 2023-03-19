c3bbbaaa056267feafc4b3df928c0d345fcb86c8
RevK <github@k.gg>
2023-03-19 11:02:45 +0000

    esp_sntp

--- revk.c
+++ revk.c
@@ -1182,8 +1182,8 @@ static void ip_event_handler(void *arg, esp_event_base_t event_base, int32_t eve
             REVK_ERR_CHECK(esp_wifi_sta_get_ap_info(&ap));
             ESP_LOGI(TAG, "Got IP " IPSTR " from %s", IP2STR(&event->ip_info.ip), (char *) ap.ssid);
             xEventGroupSetBits(revk_group, GROUP_IP);
-            sntp_stop();
-            sntp_init();
+            esp_sntp_stop();
+            esp_sntp_init();
 #ifdef	CONFIG_REVK_MQTT
             revk_mqtt_init();
 #endif
@@ -1741,10 +1741,10 @@ void revk_boot(app_callback_t * app_callback_cb)
    char *d = strstr(revk_version, "dirty");
    if (d)
       asprintf((char **) &revk_version, "%.*s%s", d - revk_version, app->version, app->time);
-   sntp_setoperatingmode(SNTP_OPMODE_POLL);
+   esp_sntp_setoperatingmode(SNTP_OPMODE_POLL);
    setenv("TZ", tz, 1);
    tzset();
-   sntp_setservername(0, ntphost);
+   esp_sntp_setservername(0, ntphost);
    app_callback = app_callback_cb;
    {                            /* Chip ID from MAC */
       REVK_ERR_CHECK(esp_efuse_mac_get_default(revk_mac));
