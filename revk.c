094a7af28d8dc0ccc88c7357826296e14bf599c6
RevK <github@k.gg>
2023-03-19 11:07:00 +0000

    version check

--- revk.c
+++ revk.c
@@ -1182,8 +1182,13 @@ static void ip_event_handler(void *arg, esp_event_base_t event_base, int32_t eve
             REVK_ERR_CHECK(esp_wifi_sta_get_ap_info(&ap));
             ESP_LOGI(TAG, "Got IP " IPSTR " from %s", IP2STR(&event->ip_info.ip), (char *) ap.ssid);
             xEventGroupSetBits(revk_group, GROUP_IP);
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 1, 0)
             esp_sntp_stop();
             esp_sntp_init();
+#else
+            sntp_stop();
+            sntp_init();
+#endif
 #ifdef	CONFIG_REVK_MQTT
             revk_mqtt_init();
 #endif
@@ -1741,10 +1746,18 @@ void revk_boot(app_callback_t * app_callback_cb)
    char *d = strstr(revk_version, "dirty");
    if (d)
       asprintf((char **) &revk_version, "%.*s%s", d - revk_version, app->version, app->time);
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 1, 0)
    esp_sntp_setoperatingmode(SNTP_OPMODE_POLL);
+#else
+   sntp_setoperatingmode(SNTP_OPMODE_POLL);
+#endif
    setenv("TZ", tz, 1);
    tzset();
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 1, 0)
    esp_sntp_setservername(0, ntphost);
+#else
+   sntp_setservername(0, ntphost);
+#endif
    app_callback = app_callback_cb;
    {                            /* Chip ID from MAC */
       REVK_ERR_CHECK(esp_efuse_mac_get_default(revk_mac));
