9f6f15dca54622d08fff8bfd9a2ae35da3f643d1
RevK <github@k.gg>
2023-07-03 10:15:04 +0100

    stack

--- lwmqtt.c
+++ lwmqtt.c
@@ -285,7 +285,7 @@ lwmqtt_client (lwmqtt_client_config_t * config)
    xSemaphoreGive (handle->mutex);
    handle->running = 1;
    TaskHandle_t task_id = NULL;
-   xTaskCreate (client_task, "mqtt-client", 5 * 1024, (void *) handle, 2, &task_id);
+   xTaskCreate (client_task, "mqtt-client", 4 * 1024, (void *) handle, 2, &task_id);
    return handle;
 }
 
