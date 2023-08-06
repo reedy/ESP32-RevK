ee982032cc4ce7f9d8be31b77203bd5bfe98c212
RevK <github@k.gg>
2023-08-06 15:35:09 +0100

    Clean fail

--- lwmqtt.c
+++ lwmqtt.c
@@ -578,7 +578,6 @@ lwmqtt_loop (lwmqtt_t handle)
          if (p[1])
          {                      // Failed
             ESP_LOGI (TAG, "Connect failed %s:%d code %d", handle->hostname, handle->port, p[1]);
-            handle->running = 0;
             handle->failed = (p[0] > 7 ? 7 : 0);;
          } else
          {
