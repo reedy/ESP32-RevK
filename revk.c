5d1694e81b913b3b23ddfbc9db03449cb2b32e67
RevK <github@k.gg>
2023-12-07 10:34:42 +0000

    more stack

--- revk.c
+++ revk.c
@@ -859,7 +859,7 @@ mesh_init (void)
       REVK_ERR_CHECK (esp_mesh_disable_ps ());
       if (meshmax == 1 || meshroot)
          esp_mesh_set_type (MESH_ROOT); // We are forcing root
-      revk_task ("mesh", mesh_task, NULL, 4);
+      revk_task ("mesh", mesh_task, NULL, 5);
    }
    REVK_ERR_CHECK (esp_mesh_start ());
 }
