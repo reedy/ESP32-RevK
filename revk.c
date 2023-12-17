f3e7bc02fe7124160dbbdf4449181312f46a3f24
RevK <github@k.gg>
2023-12-17 07:09:57 +0000

    ESP8266 first

--- revk.c
+++ revk.c
@@ -1780,6 +1780,10 @@ revk_pre_shutdown (void)
 int
 gpio_ok (uint8_t p)
 {                               // Return is bit 0 (i.e. value 1) for output OK, 1 (i.e. value 2) for input OK. bit 2 for special use (e.g. USB)
+#ifdef CONFIG_IDF_TARGET_ESP8266
+   // 8266 has GPIOs 0...16, allow any use
+   return (p <= 16) ? 3 : 0;
+#endif
 #ifdef	CONFIG_IDF_TARGET_ESP32
    if (p > 39)
       return 0;
@@ -1811,10 +1815,6 @@ gpio_ok (uint8_t p)
       return 0;
 #endif
    return 3;                    // All input and output
-#endif
-#ifdef CONFIG_IDF_TARGET_ESP8266
-   // 8266 has GPIOs 0...16, allow any use
-   return (p <= 16) ? 3 : 0;
 #endif
 }
 
