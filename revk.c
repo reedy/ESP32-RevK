f7df5147205c5384aafe9293d05b29bedbdd5b61
RevK <github@k.gg>
2024-02-06 14:33:45 +0000

    direct to app settings

--- revk.c
+++ revk.c
@@ -2703,7 +2703,7 @@ revk_web_foot (httpd_req_t * req, uint8_t home, uint8_t wifi, const char *extra)
    if (wifi && !b.disablesettings)
       revk_web_send (req, "<a href=/revk-settings"      //
 #ifdef  CONFIG_REVK_WEB_EXTRA
-                     "?level=1"
+                     "?_level=1"
 #endif
                      ">Settings</a> ");
    revk_web_send (req, appname);
