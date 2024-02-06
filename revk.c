16ad0ba9110b43a447ea65da6467d9dab9bf1d77
RevK <github@k.gg>
2024-02-06 12:58:34 +0000

    Only change if changed

--- revk.c
+++ revk.c
@@ -2791,14 +2791,14 @@ revk_web_setting (httpd_req_t * req, const char *tag, const char *field)
       )
       // Numeric
       revk_web_send (req,
-                     "<td nowrap><input id='%s' name='%s' value='%s' autocapitalize='off' autocomplete='off' spellcheck='false' size=10 autocorrect='off' placeholder='%s'></td><td>%s</td></tr>",
-                     field, field, revk_web_safe (&qs, value), place, comment);
+                     "<td nowrap><input id='%s' name='_%s' onchange=\"this.name='%s';\" value='%s' autocapitalize='off' autocomplete='off' spellcheck='false' size=10 autocorrect='off' placeholder='%s'></td><td>%s</td></tr>",
+                     field,field, field, revk_web_safe (&qs, value), place, comment);
    else
 #endif
       // Text
       revk_web_send (req,
-                     "<td nowrap colspan=2><input id='%s' name='%s' value='%s' autocapitalize='off' autocomplete='off' spellcheck='false' size=40 autocorrect='off' placeholder='%s'> %s</td></tr>",
-                     field, field, revk_web_safe (&qs, value), place, comment);
+                     "<td nowrap colspan=2><input id='%s' name='_%s' onchange=\"this.name='%s';\" value='%s' autocapitalize='off' autocomplete='off' spellcheck='false' size=40 autocorrect='off' placeholder='%s'> %s</td></tr>",
+                     field,field, field, revk_web_safe (&qs, value), place, comment);
    // Simple text input
    free (qs);
    free (value);
