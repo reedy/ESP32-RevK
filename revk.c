8e5ca8ca4c23bad0053af8b02f29783fd4a891bc
RevK <github@k.gg>
2024-02-10 13:01:42 +0000

    Trying to get working

--- revk.c
+++ revk.c
@@ -3860,7 +3860,7 @@ revk_upgrade (const char *target, jo_t j)
       esp_wifi_set_ps (WIFI_PS_NONE);   // Full wifi
 #endif
    }
-   ota_task_id = revk_task ("OTA", ota_task, url, 6);
+   ota_task_id = revk_task ("OTA", ota_task, url, 5);
    return "";
 }
 
