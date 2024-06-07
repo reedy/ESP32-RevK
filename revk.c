e1fa8bdf7ec549c61240bc4f27721896cdb32f2a
RevK <github@k.gg>
2024-06-07 11:31:18 +0100

    More debug

--- revk.c
+++ revk.c
@@ -1218,7 +1218,7 @@ revk_mqtt_init (void)
                freez (config.topic);
                return;
             }
-         ESP_LOGI (TAG, "MQTT%d %s", client, config.hostname);
+         ESP_LOGE (TAG, "MQTT%d %s %s", client, config.hostname, config.client);
          if (mqttcert[client]->len)
          {
             config.ca_cert_ref = 1;     // No need to duplicate
