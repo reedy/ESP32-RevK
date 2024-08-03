a235b1169d94cad110d8a3d9cd1a42447945cdce
RevK <github@k.gg>
2024-08-03 15:00:55 +0100

    Space

--- revk.c
+++ revk.c
@@ -927,7 +927,7 @@ revk_send_subunsub (int client, const mac_t mac, uint8_t sub)
    sprintf (id, "%02X%02X%02X%02X%02X%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
    if (client >= CONFIG_REVK_MQTT_CLIENTS || !mqtt_client[client])
       return;
-   ESP_LOGI (TAG, "MQTT%d %s%s", client, sub ? "Subscribe" : "Unsubscribe", id);
+   ESP_LOGI (TAG, "MQTT%d %s %s", client, sub ? "Subscribe" : "Unsubscribe", id);
    void subunsub (const char *prefix)
    {
       void send (const char *id)
