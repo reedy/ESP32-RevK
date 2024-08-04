953415103f32b445973f720c3646215b27864749
RevK <github@k.gg>
2024-08-04 07:53:27 +0100

    Cleaner

--- revk.c
+++ revk.c
@@ -1440,7 +1440,6 @@ ip_event_handler (void *arg, esp_event_base_t event_base, int32_t event_id, void
             {
                ESP_LOGI (TAG, "Mesh root");
                setup_ip ();     // Handles starting dhcp or setting link_down
-               revk_mqtt_init ();
             } else
             {
                ESP_LOGI (TAG, "Mesh child");
