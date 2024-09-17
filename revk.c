58e8c5bc0cdc7a4578068a281b2b147fc066f2c8
RevK <github@k.gg>
2024-09-17 10:05:14 +0100

    Hex u8

--- revk.c
+++ revk.c
@@ -3154,8 +3154,10 @@ revk_web_setting (httpd_req_t * req, const char *tag, const char *field)
       )
       // Numeric
       revk_web_send (req,
-                     "<td nowrap><input id=\"%s\" name=\"_%s\" onchange=\"this.name='%s';\" value=\"%s\" autocapitalize='off' autocomplete='off' spellcheck='false' size=10 autocorrect='off' placeholder=\"%s\" style=\"text-align:right\">%s</td><td>%s</td></tr>",
-                     field, field, field, revk_web_safe (&qs, value), place, s->gpio ? " (GPIO)" : "", comment);
+                     "<td nowrap><input %sid=\"%s\" name=\"_%s\" onchange=\"this.name='%s';\" value=\"%s\" autocapitalize='off' autocomplete='off' spellcheck='false' size=10 autocorrect='off' placeholder=\"%s\" style=\"text-align:right\">%s</td><td>%s</td></tr>",
+                     (s->base64 || s->base32
+                      || s->hex) ? "style=\"font-family:monospace\" " : "", field, field, field, revk_web_safe (&qs, value), place,
+                     s->gpio ? " (GPIO)" : "", comment);
    else
 #endif
 #ifdef  REVK_SETTINGS_HAS_JSON
