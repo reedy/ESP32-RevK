1180e0c6030a4a74490f75566644fa74d52eeea8
RevK <github@k.gg>
2024-09-24 09:23:32 +0100

    First HA config

--- include/halib.h
+++ include/halib.h
@@ -1,4 +1,19 @@
 // Home Assistant support / config librrary
 #ifdef	CONFIG_REVK_HALIB
 
+typedef struct {
+	const char *id;	// tag/id no spaces
+	const char *name;	// name
+	const char *type;	// type
+	const char *unit;	// unit (set for stat logic)
+	const char *stat;	// stat topic (default main status)
+	const char *field;	// field name (default id)
+
+	const char *icon;	// icon
+} ha_config_t;
+
+// Sensor
+#define ha_config_sensor(...)  ha_config_sensor_opts((ha_config_t){__VA_ARGS__})
+const char *ha_config_sensor_opts(ha_config_t);
+
 #endif
