88f0bd82852cee1f0c74f88b7f7b782a82b9a838
RevK <github@k.gg>
2025-01-01 07:34:01 +0000

    Try 6 and 4 separately

--- lwmqtt.c
+++ lwmqtt.c
@@ -780,7 +780,7 @@ client_task (void *pvParameters)
             {
                if (handle->sock >= 0)
                   return 1;     // connected already
-             struct addrinfo base = { ai_family: ip6 ? AF_INET6 : AF_UNSPEC, ai_socktype:SOCK_STREAM };
+             struct addrinfo base = { ai_family: ip6 ? AF_INET6 : AF_INET, ai_socktype:SOCK_STREAM };
                struct addrinfo *a = 0,
                   *p = NULL;
                if (!getaddrinfo (hostname, sport, &base, &a) && a)
